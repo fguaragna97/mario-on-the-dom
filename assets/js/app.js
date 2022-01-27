af105c75b95e318ee15bd632392a39ad7e9340e7
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2022-01-26 17:48:50 -0800

    Franco mario game

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,41 @@ function isTouching(a, b) {
     aRect.left > bRect.left + bRect.width
   );
 }
+
+function Random(max = 1) {
+  const a = Math.floor(Math.random() * max);
+  const b = Math.floor(Math.random() * max);
+  return [a, b];
+}
+let score = 0;
+
+window.addEventListener("keydown", (event) => {
+  let mario = document.getElementById("avatar");
+  let coin = document.getElementById("coin");
+  const left = parseInt(mario.style.left || "0px");
+  const top = parseInt(mario.style.top || "0px");
+
+  switch (event.key) {
+    case "ArrowUp":
+      mario.style.top = top - 50 + "px";
+      break;
+    case "ArrowRight":
+      mario.style.left = left + 50 + "px";
+      break;
+    case "ArrowDown":
+      mario.style.top = top + 50 + "px";
+      break;
+    case "ArrowLeft":
+      mario.style.left = left - 50 + "px";
+      break;
+    default:
+    // code block
+  }
+
+  if (isTouching(mario, coin) === true) {
+    coin.style.top = Random(500)[0] + "px";
+    coin.style.left = Random(500)[1] + "px";
+    score = score + 1;
+    console.log(score);
+  }
+});
